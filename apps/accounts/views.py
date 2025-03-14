8d30417244a5017be69c942f2997c39fbff250a8
Thibault Guilhem <thibault.guilhem@gmail.com>
2025-03-14 23:48:41 +0100

    Added views and URLs for user authentication, including login, registration and logout. Updated template parameters to include a basic template.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import User
 
-# Create your views here.
+class SignUpView(CreateView):
+    model = User
+    template_name = 'accounts/signup.html'
+    fields = ['username', 'email', 'password']
+    success_url = reverse_lazy('accounts:login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        return super().form_valid(form)
